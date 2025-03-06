2b188bab76132082bed296a16eb0070dab5b5370
fr0g-z <martinafarrugia6@gmail.com>
2025-03-06 15:59:11 +0100

    player respawn and health text

--- Assets/script/Health.cs
+++ Assets/script/Health.cs
@@ -1,29 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Health : MonoBehaviour
 {
     public int points = 5;
+    public Vector3 respawnPosition;
+    public TMP_Text healthText;
 
+
+    private void Start()
+    {
+        respawnPosition = transform.position;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("trap"))
         {
             Damage(1);
         }
-        
+        if (other.CompareTag("checkpoint"))
+        {
+            respawnPosition = transform.position;
+            // to respawn in the middle use this:
+            //respawnPosition = other.transform.position;
+            //respawnPosition.y = transform.position.y;     - this is to respawn on the top instead of directly in the middle
+        }
     }
     //to remove health points
     private void Damage(int value)
     {
+       
         points = points - value;
+        healthText.text = $"<b>health:</b> {points}";
+        //dollar sign is used for cominging things in a bracket
+
+
+        transform.position = respawnPosition;
+
+        // points = 5;
+
         if (points < 1)
         {
-            //homework 
-            // do not destroy move the player tp start and reset points to 5
-
+           
+         
+            
             Destroy(gameObject);
+           
         }
     }
 }
--- Assets/script/PlayerCollection.cs
+++ Assets/script/PlayerCollection.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerCollection : MonoBehaviour
 {
+    //add a scoring system here
     private void OnTriggerEnter(Collider other)
     {
         //only destroy if collectable
@@ -12,6 +13,7 @@ public class PlayerCollection : MonoBehaviour
         {
             Destroy(other.gameObject);
         }
+
 
     }
 }
